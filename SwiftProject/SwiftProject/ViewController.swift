1f536064fab3a9ffe77413d0d2be0cc1e46f15df
devinlee <i@lishujun.com>
2017-05-28 19:39:44 +0800

    初始化

---
+++ SwiftProject/SwiftProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftProject
+//
+//  Created by Devin Lee on 2017/5/28.
+//  Copyright © 2017年 devin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
